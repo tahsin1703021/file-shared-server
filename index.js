14e28eb0cc826454a55f276e66aed438978c3bd1
tahsin1703021 <safasadaf3@gmail.com>
2025-04-27 03:48:21 +0600

    updated forbidden restriction on non proxy requests

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ import StorageManager from './src/components/storage.manager.js';
 
 const PORT = process.env.PORT || 3000;
 const storageProvider = process.env.STORAGE_PROVIDER;
+const allowedHost = [process.env.ALLOWED_HOST];
 
 
 app.use(json());
+app.use((req, res, next) => {
+  const clientIp = req.headers['x-forwarded-for'] || null;
+  
+  if (!clientIp || !allowedHost.includes(clientIp)) {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
+
+  next();
+});
 
 // Import the StorageManager class
 const storageManager = new StorageManager(storageProvider);
